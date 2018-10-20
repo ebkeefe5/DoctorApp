925901b14a6ea92e73f8f33be18a4a1ee85aa942
Eric Keefe <ebkeefe@wu-124804.local>
2018-10-19 18:27:31 -0700

    Initial Commit

---
+++ GamePortion/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GamePortion
+//
+//  Created by Eric Keefe on 10/19/18.
+//  Copyright © 2018 Eric Keefe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
