33471023b268daa4a6c1ea5c9098a4a38852904d
Eric Keefe <ebkeefe5@gmail.com>
2019-02-12 16:52:22 -0800

    GitHub test added a comment in questionsViewController

--- GamePortion/QuestionsViewController.swift
+++ GamePortion/QuestionsViewController.swift
@@ -14,6 +14,7 @@ class QuestionsViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        //TESTING A COMMENT PUSH TO GITHUB
     }
     
 
